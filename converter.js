b3d1b80a351441fdfaff97798e568062bf930d26
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-07-21 09:38:52 -0500

    updated js to submit on enter keyup

--- converter.js
+++ converter.js
@@ -22,6 +22,20 @@ let celsius = document.getElementById("celsius");
 let fahrenheit = document.getElementById("fahrenheit");
 let input = document.getElementById("userInput");
 
+celsius.addEventListener('keyup', function (pushEnter) {
+        //______.which tells javascript which key. 13 = enter. If I put other key codes, that key would work instead.
+        if (pushEnter.which === 13) {
+        convertThatTemp();
+        }
+    })
+
+fahrenheit.addEventListener('keyup', function (pushEnter) {
+        //______.which tells javascript which key. 13 = enter. If I put other key codes, that key would work instead.
+        if (pushEnter.which === 13) {
+        convertThatTemp();
+        }
+    })
+
 
 function convertThatTemp() {
             	 input = Number(input.value);
@@ -31,23 +45,23 @@ function convertThatTemp() {
 	            	else if (celsius.checked){
 					    input = parseInt((input - 32)/1.8);
 					    	if (input > 32){
-					    		output.innerHTML = `<p class="red">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="red">Your converted temp is ${input} Degrees Celsius</p>`;
 					    	}
 					    	else if(input < 0){
-					    		output.innerHTML = `<p class="blue">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="blue">Your converted temp is ${input} Degrees Celsius</p>`;
 					    	}else {
-					    		output.innerHTML = `<p class="green">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="green">Your converted temp is ${input} Degrees Celsius</p>`;
 					    	}
 				}
 					else if(fahrenheit.checked) {
 					   	input = parseInt((input * 1.8)+ 32);
 					   		if (input > 90){
-					    		output.innerHTML = `<p class="red">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="red">Your converted temp is ${input} Degrees Fahrenheit</p>`;
 					    	}
 					    	else if(input < 32){
-					    		output.innerHTML = `<p class="blue">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="blue">Your converted temp is ${input} °F</p>`;
 					    	}else {
-					    		output.innerHTML = `<p class="green">Your converted temp is ${input}</p>`;
+					    		output.innerHTML = `<p class="green">Your converted temp is ${input} °F</p>`;
 					    	}
 				}
     		
